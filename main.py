50e87704c43d72bd4e50d1009342d17f6e760efe
rehamrafaat202 <rehamrafaat202@gmail.com>
2021-12-07 20:20:48 +0200

    madlib

---
+++ main.py
@@ -0,0 +1,14 @@
+color=input("Enter the color: ")
+plural_noun=input("Enter the plural noun: ")
+adjective=input("Enter the adjective: ")
+
+print("treea are "+color)
+print(plural_noun+" are mean")
+print("please keep it "+adjective)
+# def person_details(name,age):
+#     print("hello "+name +" your age is "+str(age))
+#
+# name=input("Enter your name: ")
+# age=input("Enter your age: ")
+# int(age)
+# person_details(name,age)
